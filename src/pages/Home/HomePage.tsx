5f32dd199abe5cf66b6b750b17679524a0dbb72c
Harry Gupenec <Spektar001@mail.ru>
2023-03-21 10:45:13 +0300

    feat: add method "componentWillUnmount" and moved the "Search" input to a separate component

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,36 +1,15 @@
-import React, { ChangeEvent, Component } from 'react';
+import React, { Component } from 'react';
 import './HomePage.css';
 import { Product } from '../../components/Product';
-import products from '../../components/Data/productsData';
-import { State, Props } from '../../components/Types/types';
+import products from '../../components/data/productsData';
+import Search from '../../components/Search/Search';
 
-export class ProductsPage extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { text: '' };
-    this.changeHandler = this.changeHandler.bind(this);
-  }
-
-  changeHandler(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ text: event.target.value });
-    localStorage.setItem('inputText', JSON.stringify(event.target.value));
-  }
-
+export class ProductsPage extends Component {
   render() {
     return (
       <div className="home">
         <div className="home__search_box">
-          <input
-            value={
-              localStorage.getItem('inputText') !== null
-                ? JSON.parse(localStorage.getItem('inputText') || '')
-                : this.state.text
-            }
-            onChange={this.changeHandler}
-            className="home__input"
-            type="text"
-            placeholder="Search..."
-          />
+          <Search />
           <button data-testid="find" className="home__btn">
             find
           </button>
